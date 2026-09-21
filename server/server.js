0cbf0bbeb5470abf80396e6d791fbee4d394f57c
sauravhavhad <saurabhavhad15@gmail.com>
2026-09-22 01:38:45 +0530

    Add vercel rewrite

--- server/server.js
+++ server/server.js
@@ -13,8 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
+    // Connect to MongoDB first
     await connectDB();
 
+    // Start server
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
